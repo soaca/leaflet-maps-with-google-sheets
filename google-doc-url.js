178eac8338dfc0271a5fc0b2633484d0efcaf108
soaca <39529492+soaca@users.noreply.github.com>
2019-04-13 10:22:01 +0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/16e3JTkbFD6mUe1qdX8e71HuQ25jE-gqBrJLuEiXRC9E/edit#gid=0';
